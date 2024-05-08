6a91caab35546e27333770a9b9040d2c98a35072
RockyHorrorFreakShow <145370417+RockyHorrorFreakShow@users.noreply.github.com>
2024-05-08 17:25:07 +0100

    Level 1-3 whitebox

---
+++ Assets/Scripts 1/resetlevel.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class resetlevel : MonoBehaviour
+{
+    // Assign this in the Inspector to the button you want to trigger the level reset
+    public KeyCode resetKey = KeyCode.R;
+
+    void Update()
+    {
+        // Check if the assigned key is pressed
+        if (Input.GetKeyDown(resetKey))
+        {
+            // Call the Reset function
+            ResetLvl();
+        }
+    }
+
+    public void ResetScene()
+    {
+        // Call the Reset function when the UI button is clicked
+        ResetLvl();
+    }
+
+    void ResetLvl()
+    {
+        // Get the current scene index
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Reload the current scene
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
